a11cddf8c6447171370ab2edc9d4c5500149716a
Svenlaa <mail@svenlaa.com>
2023-12-16 21:10:57 +0100

    Update actor page layout for mobile view

--- resources/views/actors/show.blade.php
+++ resources/views/actors/show.blade.php
@@ -1,6 +1,6 @@
-<x-app-layout class="max-w-7xl mx-auto px-4 relative flex flex-row gap-4">
+<x-app-layout class="max-w-7xl flex-col mx-auto px-4 relative items-center flex sm:flex-row gap-4">
     <div
-        class="flex-none sticky top-0 h-min bg-white w-64 flex flex-col rounded-b-md drop-shadow-md">
+        class="flex-none w-full sticky top-0 h-min bg-white sm:w-64 flex flex-col items-center rounded-b-md drop-shadow-md">
         <div class="pt-6 px-8 sticky w-64 rounded-b-md drop-shadow-md">
             <img src="{{Storage::temporaryUrl($actor->avatar_path, now()->addHour(1))}}"
                  class="rounded-xl bg-[url('{{config('app.url')}}/images/avatar.webp')]"/>
