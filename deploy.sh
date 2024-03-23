32f788c09174a2537c58443ba698a14e4592ee74
Svenlaa <mail@svenlaa.com>
2024-03-23 15:58:11 +0100

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+php artisan down --render="errors::503"
+
+git reset --hard origin/master
+git pull
+VERSION=$(git rev-parse --short HEAD)
+sed -i~ /^APP_VERSION=/s/=.*/="${VERSION}"/ .env
+
+composer install --optimize-autoloader -no
+php artisan migrate --force
+
+pnpm i -g pnpm
+pnpm i --frozen-lockfile
+pnpm build
+
+php artisan optimize:clear
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan up
